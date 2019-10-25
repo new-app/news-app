fded1f952b8de8ff2ad87661c058cfeeb22cd9af
zxl1997 <959231531@qq.com>
2019-10-25 16:46:05 +0800

    原始模板

---
+++ news-app/pages/my/my.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="my">
+		我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+ .my{
+	 padding-top: 50upx;
+ }
+</style>
